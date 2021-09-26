f13980e4c9dbb2f47ed5a66209a6938928795efb
Tommo J. Phillips <tommojphillips@gmail.com>
2021-09-26 17:24:42 +1000

    implemented base mod

--- SecureWarningTriangle/SecureWarningTriangleMod.cs
+++ SecureWarningTriangle/SecureWarningTriangleMod.cs
@@ -1,6 +1,6 @@
 ﻿using MSCLoader;
 using System;
-using System.Collections.Generic;
+using TommoJProductions.ModApi;
 using TommoJProductions.ModApi.Attachable;
 using UnityEngine;
 using static TommoJProductions.ModApi.Attachable.Part;
@@ -17,19 +17,25 @@ namespace SecureWarningTriangle
 
         private const string FILE_NAME = "SecureWarningTriangle.txt";
 
+        private GameObject warningTriangleGo;
         private Part warningTrianglePart;
         private PartSaveInfo warningTriangleLoadedSaveInfo;
+        private bool warningTriangleInitialized = false;
 
         public override void OnLoad()
         {
             warningTriangleLoadedSaveInfo = loadData();
-            GameObject satsuma = GameObject.Find("SATSUMA(557kg, 248)");
-            GameObject warningTri = GameObject.Find("warning triangle(Clone)");
-            Trigger trigger = new Trigger("WarningTriangleTrigger", satsuma, new Vector3(0, -0.053f, -1.45f));
-            warningTrianglePart = warningTri.AddComponent<Part>();
-            AssemblyTypeJointSettings atjs = new AssemblyTypeJointSettings(satsuma.GetComponent<Rigidbody>()) { breakForce = 250 };
-            PartSettings ps = new PartSettings() { assembleType = AssembleType.joint, assemblyTypeJointSettings = atjs, setPositionRotationOnInitialisePart = false };
-            warningTrianglePart.initPart(warningTriangleLoadedSaveInfo, ps, trigger);
+            warningTriangleGo = GameObject.Find("warning triangle(Clone)");
+            PlayMakerFSM removalFsm = warningTriangleGo.GetPlayMaker("Removal");
+
+            if (removalFsm.enabled)
+            {
+                warningTriangleGo.FsmInject("Remove part", initWarningTriangle);
+            }
+            else
+            {
+                initWarningTriangle();
+            }
             ModConsole.Print("SecureWarningTriangle: Loaded");
         }
 
@@ -37,14 +43,29 @@ namespace SecureWarningTriangle
         {
             try
             {
-                SaveLoad.SerializeSaveFile(this, warningTrianglePart.getSaveInfo(), FILE_NAME);
+                if (!warningTrianglePart)
+                    SaveLoad.SerializeSaveFile(this, warningTrianglePart.getSaveInfo(), FILE_NAME);
             }
             catch (Exception ex)
             {
                 ModConsole.Error("<b>[SecureWarningTriangle]</b> - an error occured while attempting to save part info.. see: " + ex.ToString());
             }
         }
-
+        private void initWarningTriangle()
+        {
+            if (!warningTriangleInitialized)
+            {
+                warningTriangleInitialized = true;
+                GameObject satsuma = GameObject.Find("SATSUMA(557kg, 248)");
+                GameObject backPanel = GameObject.Find("back panel(Clone)");
+                Trigger triggerTrunk = new Trigger("WarningTriangleTrunkTrigger", satsuma, new Vector3(0.4419999f, 0.132f, -1.582f), new Vector3(270f, 50.89845f, 0));
+                Trigger triggerBackPanel = new Trigger("WarningTriangleBackPanelTrigger", backPanel, new Vector3(-0.4130001f, 0.09600005f, 0.162f), new Vector3(355, 0f, 0f));
+                warningTrianglePart = warningTriangleGo.AddComponent<Part>();
+                PartSettings ps = new PartSettings() { assembleType = AssembleType.static_rigidibodySetKinematic, setPositionRotationOnInitialisePart = false, installedPartToLayer = LayerMasksEnum.DontCollide };
+                warningTrianglePart.initPart(warningTriangleLoadedSaveInfo, ps, triggerTrunk, triggerBackPanel);
+                ModConsole.Print("SecureWarningTriangle: Initialized");
+            }
+        }
         private PartSaveInfo loadData() 
         {
             try
